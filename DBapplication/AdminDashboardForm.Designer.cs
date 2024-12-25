0ba27309e3c037400ca843c07ff54cdef9d53966
Ali_Said <alisaid14173@gmail.com>
2024-12-25 13:31:16 +0200

    Admin Dashboard Design

---
+++ DBapplication/AdminDashboardForm.Designer.cs
@@ -0,0 +1,398 @@
+﻿namespace DBapplication
+{
+    partial class AdminDashboardForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pnlNavigation = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnUserManagement = new System.Windows.Forms.Button();
+            this.btnReporting = new System.Windows.Forms.Button();
+            this.btnClubManagement = new System.Windows.Forms.Button();
+            this.btnLocationManagement = new System.Windows.Forms.Button();
+            this.btnEventManagement = new System.Windows.Forms.Button();
+            this.pnlQuickStats = new System.Windows.Forms.Panel();
+            this.lblOverview = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblTotalUsers = new System.Windows.Forms.Label();
+            this.lblTotalAdmins = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblTotalFaculty = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblTotalStudents = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblTotalEvents = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblTotalClubs = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblTotalLocations = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnCreateEvent = new System.Windows.Forms.Button();
+            this.btnGenerateReport = new System.Windows.Forms.Button();
+            this.btnViewUsers = new System.Windows.Forms.Button();
+            this.btnLogout = new System.Windows.Forms.Button();
+            this.pnlNavigation.SuspendLayout();
+            this.pnlQuickStats.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pnlNavigation
+            // 
+            this.pnlNavigation.Controls.Add(this.btnEventManagement);
+            this.pnlNavigation.Controls.Add(this.btnLocationManagement);
+            this.pnlNavigation.Controls.Add(this.btnClubManagement);
+            this.pnlNavigation.Controls.Add(this.btnReporting);
+            this.pnlNavigation.Controls.Add(this.btnUserManagement);
+            this.pnlNavigation.Controls.Add(this.label1);
+            this.pnlNavigation.Location = new System.Drawing.Point(10, 19);
+            this.pnlNavigation.Name = "pnlNavigation";
+            this.pnlNavigation.Size = new System.Drawing.Size(267, 350);
+            this.pnlNavigation.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(81, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(110, 16);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Navigation Panel";
+            // 
+            // btnUserManagement
+            // 
+            this.btnUserManagement.Location = new System.Drawing.Point(42, 41);
+            this.btnUserManagement.Name = "btnUserManagement";
+            this.btnUserManagement.Size = new System.Drawing.Size(182, 42);
+            this.btnUserManagement.TabIndex = 1;
+            this.btnUserManagement.Text = "User Management";
+            this.btnUserManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnReporting
+            // 
+            this.btnReporting.Location = new System.Drawing.Point(42, 300);
+            this.btnReporting.Name = "btnReporting";
+            this.btnReporting.Size = new System.Drawing.Size(182, 42);
+            this.btnReporting.TabIndex = 2;
+            this.btnReporting.Text = "Reporting";
+            this.btnReporting.UseVisualStyleBackColor = true;
+            // 
+            // btnClubManagement
+            // 
+            this.btnClubManagement.Location = new System.Drawing.Point(42, 165);
+            this.btnClubManagement.Name = "btnClubManagement";
+            this.btnClubManagement.Size = new System.Drawing.Size(182, 42);
+            this.btnClubManagement.TabIndex = 3;
+            this.btnClubManagement.Text = "Club Management";
+            this.btnClubManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnLocationManagement
+            // 
+            this.btnLocationManagement.Location = new System.Drawing.Point(42, 234);
+            this.btnLocationManagement.Name = "btnLocationManagement";
+            this.btnLocationManagement.Size = new System.Drawing.Size(182, 42);
+            this.btnLocationManagement.TabIndex = 4;
+            this.btnLocationManagement.Text = "Location Management";
+            this.btnLocationManagement.UseVisualStyleBackColor = true;
+            // 
+            // btnEventManagement
+            // 
+            this.btnEventManagement.Location = new System.Drawing.Point(42, 99);
+            this.btnEventManagement.Name = "btnEventManagement";
+            this.btnEventManagement.Size = new System.Drawing.Size(182, 42);
+            this.btnEventManagement.TabIndex = 5;
+            this.btnEventManagement.Text = "Event Management";
+            this.btnEventManagement.UseVisualStyleBackColor = true;
+            // 
+            // pnlQuickStats
+            // 
+            this.pnlQuickStats.Controls.Add(this.lblTotalLocations);
+            this.pnlQuickStats.Controls.Add(this.label8);
+            this.pnlQuickStats.Controls.Add(this.lblTotalClubs);
+            this.pnlQuickStats.Controls.Add(this.label6);
+            this.pnlQuickStats.Controls.Add(this.lblTotalEvents);
+            this.pnlQuickStats.Controls.Add(this.label4);
+            this.pnlQuickStats.Controls.Add(this.lblTotalStudents);
+            this.pnlQuickStats.Controls.Add(this.label9);
+            this.pnlQuickStats.Controls.Add(this.lblTotalFaculty);
+            this.pnlQuickStats.Controls.Add(this.label7);
+            this.pnlQuickStats.Controls.Add(this.lblTotalAdmins);
+            this.pnlQuickStats.Controls.Add(this.label5);
+            this.pnlQuickStats.Controls.Add(this.lblTotalUsers);
+            this.pnlQuickStats.Controls.Add(this.label2);
+            this.pnlQuickStats.Controls.Add(this.lblOverview);
+            this.pnlQuickStats.Location = new System.Drawing.Point(289, 19);
+            this.pnlQuickStats.Name = "pnlQuickStats";
+            this.pnlQuickStats.Size = new System.Drawing.Size(696, 217);
+            this.pnlQuickStats.TabIndex = 1;
+            // 
+            // lblOverview
+            // 
+            this.lblOverview.AutoSize = true;
+            this.lblOverview.Location = new System.Drawing.Point(32, 22);
+            this.lblOverview.Name = "lblOverview";
+            this.lblOverview.Size = new System.Drawing.Size(63, 16);
+            this.lblOverview.TabIndex = 0;
+            this.lblOverview.Text = "Overview";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(32, 63);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 16);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Total Users: ";
+            // 
+            // lblTotalUsers
+            // 
+            this.lblTotalUsers.AutoSize = true;
+            this.lblTotalUsers.Location = new System.Drawing.Point(121, 63);
+            this.lblTotalUsers.Name = "lblTotalUsers";
+            this.lblTotalUsers.Size = new System.Drawing.Size(38, 16);
+            this.lblTotalUsers.TabIndex = 2;
+            this.lblTotalUsers.Text = "Total";
+            // 
+            // lblTotalAdmins
+            // 
+            this.lblTotalAdmins.AutoSize = true;
+            this.lblTotalAdmins.Location = new System.Drawing.Point(276, 63);
+            this.lblTotalAdmins.Name = "lblTotalAdmins";
+            this.lblTotalAdmins.Size = new System.Drawing.Size(38, 16);
+            this.lblTotalAdmins.TabIndex = 4;
+            this.lblTotalAdmins.Text = "Total";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(178, 63);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(92, 16);
+            this.label5.TabIndex = 3;
+            this.label5.Text = "Total Admins: ";
+            // 
+            // lblTotalFaculty
+            // 
+            this.lblTotalFaculty.AutoSize = true;
+            this.lblTotalFaculty.Location = new System.Drawing.Point(482, 63);
+            this.lblTotalFaculty.Name = "lblTotalFaculty";
+            this.lblTotalFaculty.Size = new System.Drawing.Size(38, 16);
+            this.lblTotalFaculty.TabIndex = 6;
+            this.lblTotalFaculty.Text = "Total";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(326, 63);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(150, 16);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Total Faculty Members: ";
+            // 
+            // lblTotalStudents
+            // 
+            this.lblTotalStudents.AutoSize = true;
+            this.lblTotalStudents.Location = new System.Drawing.Point(642, 63);
+            this.lblTotalStudents.Name = "lblTotalStudents";
+            this.lblTotalStudents.Size = new System.Drawing.Size(38, 16);
+            this.lblTotalStudents.TabIndex = 8;
+            this.lblTotalStudents.Text = "Total";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(537, 63);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(99, 16);
+            this.label9.TabIndex = 7;
+            this.label9.Text = "Total Students: ";
+            // 
+            // lblTotalEvents
+            // 
+            this.lblTotalEvents.AutoSize = true;
+            this.lblTotalEvents.Location = new System.Drawing.Point(126, 100);
+            this.lblTotalEvents.Name = "lblTotalEvents";
+            this.lblTotalEvents.Size = new System.Drawing.Size(38, 16);
+            this.lblTotalEvents.TabIndex = 10;
+            this.lblTotalEvents.Text = "Total";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(32, 100);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Total Events: ";
+            // 
+            // lblTotalClubs
+            // 
+            this.lblTotalClubs.AutoSize = true;
+            this.lblTotalClubs.Location = new System.Drawing.Point(119, 135);
+            this.lblTotalClubs.Name = "lblTotalClubs";
+            this.lblTotalClubs.Size = new System.Drawing.Size(38, 16);
+            this.lblTotalClubs.TabIndex = 12;
+            this.lblTotalClubs.Text = "Total";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(32, 135);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Total Clubs: ";
+            // 
+            // lblTotalLocations
+            // 
+            this.lblTotalLocations.AutoSize = true;
+            this.lblTotalLocations.Location = new System.Drawing.Point(143, 170);
+            this.lblTotalLocations.Name = "lblTotalLocations";
+            this.lblTotalLocations.Size = new System.Drawing.Size(38, 16);
+            this.lblTotalLocations.TabIndex = 14;
+            this.lblTotalLocations.Text = "Total";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(32, 170);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 16);
+            this.label8.TabIndex = 13;
+            this.label8.Text = "Total Locations: ";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnViewUsers);
+            this.panel1.Controls.Add(this.btnGenerateReport);
+            this.panel1.Controls.Add(this.btnCreateEvent);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(289, 249);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(696, 120);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(32, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 16);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Shortcuts";
+            // 
+            // btnCreateEvent
+            // 
+            this.btnCreateEvent.Location = new System.Drawing.Point(74, 48);
+            this.btnCreateEvent.Name = "btnCreateEvent";
+            this.btnCreateEvent.Size = new System.Drawing.Size(148, 42);
+            this.btnCreateEvent.TabIndex = 6;
+            this.btnCreateEvent.Text = "Create New Event";
+            this.btnCreateEvent.UseVisualStyleBackColor = true;
+            // 
+            // btnGenerateReport
+            // 
+            this.btnGenerateReport.Location = new System.Drawing.Point(266, 48);
+            this.btnGenerateReport.Name = "btnGenerateReport";
+            this.btnGenerateReport.Size = new System.Drawing.Size(148, 42);
+            this.btnGenerateReport.TabIndex = 7;
+            this.btnGenerateReport.Text = "Generate A Report";
+            this.btnGenerateReport.UseVisualStyleBackColor = true;
+            // 
+            // btnViewUsers
+            // 
+            this.btnViewUsers.Location = new System.Drawing.Point(471, 48);
+            this.btnViewUsers.Name = "btnViewUsers";
+            this.btnViewUsers.Size = new System.Drawing.Size(148, 42);
+            this.btnViewUsers.TabIndex = 8;
+            this.btnViewUsers.Text = "View Users";
+            this.btnViewUsers.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            this.btnLogout.Location = new System.Drawing.Point(855, 375);
+            this.btnLogout.Name = "btnLogout";
+            this.btnLogout.Size = new System.Drawing.Size(114, 27);
+            this.btnLogout.TabIndex = 3;
+            this.btnLogout.Text = "Logout";
+            this.btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // AdminDashboardForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 414);
+            this.Controls.Add(this.btnLogout);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pnlQuickStats);
+            this.Controls.Add(this.pnlNavigation);
+            this.Name = "AdminDashboardForm";
+            this.Text = "AdminDashboardForm";
+            this.pnlNavigation.ResumeLayout(false);
+            this.pnlNavigation.PerformLayout();
+            this.pnlQuickStats.ResumeLayout(false);
+            this.pnlQuickStats.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlNavigation;
+        private System.Windows.Forms.Button btnEventManagement;
+        private System.Windows.Forms.Button btnLocationManagement;
+        private System.Windows.Forms.Button btnClubManagement;
+        private System.Windows.Forms.Button btnReporting;
+        private System.Windows.Forms.Button btnUserManagement;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel pnlQuickStats;
+        private System.Windows.Forms.Label lblOverview;
+        private System.Windows.Forms.Label lblTotalLocations;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblTotalClubs;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblTotalEvents;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblTotalStudents;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label lblTotalFaculty;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblTotalAdmins;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblTotalUsers;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button btnViewUsers;
+        private System.Windows.Forms.Button btnGenerateReport;
+        private System.Windows.Forms.Button btnCreateEvent;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnLogout;
+    }
+}
